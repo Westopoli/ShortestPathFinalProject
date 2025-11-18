7959aaab21e6c59a8f3393adadaf9c8a456098d1
jrivros1507 <kwxag1507@yahoo.com>
2025-11-17 19:50:55 -0500

    Update Edge.cpp

--- Edge.cpp
+++ Edge.cpp
@@ -7,12 +7,13 @@
    pairs. The intent is that for every Edge object
    instantiated, a relationship will exist between that
    object's identifier and each of its neighbor key/value
-   pairs. Effectively, a list of all available neighbor
-   vertices for any specified vertex will be supplied, as well
-   as the distance between the source-vertex and destination-
-   vertex. For example: map A = {(B, 5), (C, 7)} implies that
+   pairs. Effectively, class Edge supplies a list of all
+   available neighbor vertices for any specified vertex will
+   be supplied, as well as the distance between the
+   source-vertex and destination-vertex.
+   For example: map A = {(B, 5), (C, 7)} implies that
    source-vertex A has neighbors B and C, whose edges are a
-   length of 5u and 7u respectively.                           */
+   length of 5units and 7units respectively.                 */
 
 #include "Edge.hpp"
 
@@ -25,13 +26,13 @@ Edge::Edge(){
 }
 
 /* Redundant, but satisfies course requirement.
-   Uses overloaded clear() to invoke STL map.clear(). */
+   Abstracts the STL map and its clear() function. */
 Edge::~Edge(){
     clear();
 }
 
 /* Because STL map.insert() prevents the entry of elements with
-   duplicate keys, this function safely ignores dupicate keys.  */
+   duplicate keys, this function safely ignores duplicate keys.  */
 void Edge::insert(const std::string& vertex, unsigned long distance){
     neighbors.insert({vertex, distance});
     return;
@@ -43,22 +44,22 @@ const std::map<std::string, unsigned long>& Edge::get_neighbors() const{
     return neighbors;
 }
 
-/* Although STL map.size() may return number > int-range, main() will
+/* Although STL map.size() may exceed int-range, main() will
    restrict the number of neighbors to stay within int bounds.        */
 int Edge::get_size() const{
     return neighbors.size();
 }
 
 /* Uses STL map.erase() to remove a target neighbor/distance pair
-   once caller identifies it.                                     */
+   once the caller identifies it.                                     */
 void Edge::remove_neighbor(const std::string& target){
     neighbors.erase(target);
 }
 
-/* This function allows indirect call to STL map.clear(),
-   encapsulating logic by abstraction.                    */
+/* Abstracts the STL map and its clear() function. */
 void Edge::clear(){
     neighbors.clear();
     return;
 
 }
+
